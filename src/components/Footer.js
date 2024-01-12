66771ce5d8f818ea2fadfd6a3ac729fb6f3b9e2b
rajeshofficial <rscdraj@gmail.com>
2024-01-12 12:00:05 +0000

    link updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -33,7 +33,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/"
+                href="https://twitter.com/cryptokoder/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/RajeshOfficial"
+                  href="https://twitter.com/cryptokoder"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
