4dafb4c94aeb1e3dbc4852d426f47118c6171287
rajeshofficial <rscdraj@gmail.com>
2024-01-14 12:05:51 +0000

    location updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -9,7 +9,7 @@ function AboutCard() {
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
             Hi Everyone, I am <span className="purple">Rajesh Singh  </span>
-            from <span className="purple"> Glasgow , India.</span>
+            from <span className="purple"> Glasgow , Scotland , UK</span>
             <br />
             My last experice was a contact With JEM Carpets as a frontend web developer .
             <br />
